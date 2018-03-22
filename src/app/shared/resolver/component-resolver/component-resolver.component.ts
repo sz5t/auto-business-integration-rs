4d3c2fd7d278b093cde41a015df1d05fee2c4690
zxlgrandia <grandia-4727@163.com>
2018-03-22 09:34:00 +0800

    update component-resolver

--- src/app/shared/resolver/component-resolver/component-resolver.component.ts
+++ src/app/shared/resolver/component-resolver/component-resolver.component.ts
@@ -1,24 +1,106 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  Component, ComponentFactoryResolver, ComponentRef, Input, OnChanges, OnInit, Type, ViewChild,
+  ViewContainerRef
+} from '@angular/core';
 import { _HttpClient } from '@delon/theme';
-
+import {BsnDataTableComponent} from "@shared/business/bsn-data-table/bsn-data-table.component";
+const component: { [type: string]: Type<any> } = {
+  bsnDataTable: BsnDataTableComponent
+};
 @Component({
   selector: 'cn-component-resolver',
   templateUrl: './component-resolver.component.html',
 })
-export class ComponentResolverComponent implements OnInit {
+export class ComponentResolverComponent implements OnInit, OnChanges {
   @Input() config;
+  componentRef: ComponentRef<any>;
+  @ViewChild('body', { read: ViewContainerRef }) container: ViewContainerRef;
+  menuConfig = [
+  {
+    label: '表格组件',
+    value : {},
+    children: [
+      {
+        label: '数据网格',
+        value : {
+          component:'bsnDataTable'
+        }
+      }
+    ]
+  },
+  {
+    label: '列表组件',
+    value : {},
+    children: [
+      {
+        label: '数据列表',
+        value : {}
+      }
+    ]
+  },
+  {
+    label: '树组件',
+    value : {},
+    children: [
+      {
+        label: '树组件',
+        value : {}
+      }
+    ]
+  },
+  {
+    label: '布局组件',
+    value : {},
+    children: [
+      {
+        label: '标签页',
+        value : {}
+      },
+      {
+        label: '分步页',
+        value : {}
+      },
+      {
+        label: '折叠面板',
+        value : {}
+      }
+    ]
+  }
+];
   constructor(
-      private http: _HttpClient
+      private http: _HttpClient,
+      private resolver: ComponentFactoryResolver
   ) { }
 
   ngOnInit() {
+    if(this.config){
+      this.createBsnComponent();
+    }
 
   }
 
-  createMenu($event){
-    if(3 === $event.switch){
-      // 右键创建弹出组件菜单
+  ngOnChanges() {
+    if (this.componentRef) {
+      this.componentRef.instance.config = this.config;
     }
+  }
+
+  createBsnComponent() {
+    if (!component[this.config.component]) {
+      const supportedTypes = Object.keys(component).join(', ');
+      throw new Error(
+        `Trying to use an unsupported types (${this.config.component}).Supported types: ${supportedTypes}`
+      );
+    }
+    this.container.clear();
+    const comp = this.resolver.resolveComponentFactory<any>(component[this.config.component]);
+    this.componentRef = this.container.createComponent(comp);
+    this.componentRef.instance.config = this.config;
+  }
+
+  getMenuData(event) {
+    this.config = event;
+    this.createBsnComponent();
   }
 
 }

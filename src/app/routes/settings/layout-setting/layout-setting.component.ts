1148d435426c01cd0a55e0afc8a98dac98235197
zxlgrandia <grandia-4727@163.com>
2018-03-23 15:05:09 +0800

    layout-setting load module data

--- src/app/routes/settings/layout-setting/layout-setting.component.ts
+++ src/app/routes/settings/layout-setting/layout-setting.component.ts
@@ -3,13 +3,17 @@ import {_HttpClient} from '@delon/theme';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {HttpHeaders} from "@angular/common/http";
 import {environment} from "@env/environment";
+import {ApiService} from "@core/utility/api-service";
+import {APIResource} from "@core/utility/api-resource";
+import {CacheService} from "@delon/cache";
+import {AppUser, OnlineUser} from "../../passport/login/login.component";
 @Component({
   selector: 'layout-setting',
   templateUrl: './layout-setting.component.html',
 })
 export class LayoutSettingComponent implements OnInit {
   // 加载模块数据
-  _funcOptions =  [
+  _funcOptions = [
     {
       value: 'm_1',
       label: '模块 1',
@@ -39,7 +43,7 @@ export class LayoutSettingComponent implements OnInit {
     }
   ];
   // 定义布局模版
-  _layoutOptions =  [
+  _layoutOptions = [
     {
       value: {
         title: '标题 1',
@@ -49,7 +53,7 @@ export class LayoutSettingComponent implements OnInit {
               cols: [
                 {
                   title: '区域1',
-                  span:24,
+                  span: 24,
                   size: {
                     nzXs: 24,
                     nzSm: 24,
@@ -118,7 +122,7 @@ export class LayoutSettingComponent implements OnInit {
               cols: [
                 {
                   title: '区域1',
-                  span:6,
+                  span: 6,
                   size: {
                     nzXs: 6,
                     nzSm: 6,
@@ -129,7 +133,7 @@ export class LayoutSettingComponent implements OnInit {
                 },
                 {
                   title: '区域2',
-                  span:18,
+                  span: 18,
                   size: {
                     nzXs: 18,
                     nzSm: 18,
@@ -232,7 +236,7 @@ export class LayoutSettingComponent implements OnInit {
       label: '左右结构'
     },
     {
-      value:  {
+      value: {
         title: '标题 1',
         rows: [
           {
@@ -240,7 +244,7 @@ export class LayoutSettingComponent implements OnInit {
               cols: [
                 {
                   title: '区域1',
-                  span:24,
+                  span: 24,
                   size: {
                     nzXs: 24,
                     nzSm: 24,
@@ -257,7 +261,7 @@ export class LayoutSettingComponent implements OnInit {
               cols: [
                 {
                   title: '区域2',
-                  span:24,
+                  span: 24,
                   size: {
                     nzXs: 24,
                     nzSm: 24,
@@ -368,7 +372,7 @@ export class LayoutSettingComponent implements OnInit {
               cols: [
                 {
                   title: '区域1',
-                  span:6,
+                  span: 6,
                   size: {
                     nzXs: 6,
                     nzSm: 6,
@@ -379,7 +383,7 @@ export class LayoutSettingComponent implements OnInit {
                 },
                 {
                   title: '区域2',
-                  span:18,
+                  span: 18,
                   size: {
                     nzXs: 18,
                     nzSm: 18,
@@ -387,13 +391,13 @@ export class LayoutSettingComponent implements OnInit {
                     nzLg: 18,
                     ngXl: 18
                   },
-                  rows:[
+                  rows: [
                     {
                       row: {
                         cols: [
                           {
                             title: '区域2',
-                            span:24,
+                            span: 24,
                             size: {
                               nzXs: 24,
                               nzSm: 24,
@@ -410,7 +414,7 @@ export class LayoutSettingComponent implements OnInit {
                         cols: [
                           {
                             title: '区域3',
-                            span:24,
+                            span: 24,
                             size: {
                               nzXs: 24,
                               nzSm: 24,
@@ -568,7 +572,7 @@ export class LayoutSettingComponent implements OnInit {
               cols: [
                 {
                   title: '区域1',
-                  span:24,
+                  span: 24,
                   size: {
                     nzXs: 24,
                     nzSm: 24,
@@ -585,7 +589,7 @@ export class LayoutSettingComponent implements OnInit {
               cols: [
                 {
                   title: '区域1',
-                  span:12,
+                  span: 12,
                   size: {
                     nzXs: 12,
                     nzSm: 12,
@@ -596,7 +600,7 @@ export class LayoutSettingComponent implements OnInit {
                 },
                 {
                   title: '区域1',
-                  span:12,
+                  span: 12,
                   size: {
                     nzXs: 12,
                     nzSm: 12,
@@ -749,7 +753,7 @@ export class LayoutSettingComponent implements OnInit {
               cols: [
                 {
                   title: '区域1',
-                  span:12,
+                  span: 12,
                   size: {
                     nzXs: 12,
                     nzSm: 12,
@@ -760,7 +764,7 @@ export class LayoutSettingComponent implements OnInit {
                 },
                 {
                   title: '区域1',
-                  span:12,
+                  span: 12,
                   size: {
                     nzXs: 12,
                     nzSm: 12,
@@ -777,7 +781,7 @@ export class LayoutSettingComponent implements OnInit {
               cols: [
                 {
                   title: '区域1',
-                  span:24,
+                  span: 24,
                   size: {
                     nzXs: 24,
                     nzSm: 24,
@@ -927,27 +931,40 @@ export class LayoutSettingComponent implements OnInit {
   _layoutValue;
   _formGroup: FormGroup;
   _editorConfig;
-  constructor(
-    private http: _HttpClient,
-    private formBuilder: FormBuilder
-  ) {
+  _appUser: AppUser;
+  _onlineUser: OnlineUser;
+  _applyId;
+
+  constructor(private apiService: ApiService,
+              private formBuilder: FormBuilder,
+              private cacheService: CacheService) {
+    this._onlineUser = this.cacheService.getNone<OnlineUser>('OnlineUser');
+    this._appUser = this.cacheService.getNone<AppUser>('User');
+    this._applyId = this.cacheService.getNone('ApplyId');
   }
 
   ngOnInit() {
-    /*const url = environment.SERVER_URL + 'SinoForce.AppProject.AppModuleConfig';
     const params = {
-      ProjId: '002905c7bf57c54c9e5e65ec0e5fafe8',
-      ApplyId: '3935eb43532d435398d5189d5ece0f5d',
-      PlatCustomerId: 'f2771e4c90db29439e3c986d9859dc74',
+      ProjId: this._onlineUser.ProjId,
+      PlatCustomerId: this._appUser.PlatCustomerId,
+      //ApplyId: this._applyId,
       _select: 'Id,Name,ParentId'
     };
-    const header = new HttpHeaders();
-    this.http.get<any>(url,params,{headers: header.append('Credential','5bcb946f31c54db8be3b1f19284d475b')}).subscribe(
+    this.apiService.get(APIResource.AppModuleConfig, params).subscribe(
       response => {
-        console.log(response);
-      }
-    );*/
+        const moduleList = [];
+        if (response && response.Data) {
+          this._funcOptions = this.arrayToTree(response.Data, '');
+        }
+      });
     this._formGroup = this.formBuilder.group({});
+  }
+
+
+  setModuleList(modules, id) {
+    return modules.filter(module => {
+      module.ParentId = id;
+    });
   }
 
   get controlsData() {
@@ -1002,7 +1019,7 @@ export class LayoutSettingComponent implements OnInit {
   resetForm($event: MouseEvent) {
     $event.preventDefault();
     this._formGroup.reset();
-    for(const key in this._formGroup.controls){
+    for (const key in this._formGroup.controls) {
       this._formGroup.controls[key].markAsPristine();
     }
   }
@@ -1010,7 +1027,23 @@ export class LayoutSettingComponent implements OnInit {
   _submitForm($event) {
     event.preventDefault();
     event.stopPropagation();
-    console.log(this.value);
-    console.log(this._layoutValue)
+  }
+
+  arrayToTree(data, parentid) {
+    const result = [];
+    let temp;
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].ParentId == parentid) {
+        const obj = { "label": data[i].Name, "value": data[i].Id };
+        temp = this.arrayToTree(data, data[i].Id);
+        if (temp.length > 0) {
+          obj['children'] = temp;
+        } else {
+          obj["isLeaf"] = true;
+        }
+        result.push(obj);
+      }
+    }
+    return result;
   }
 }
